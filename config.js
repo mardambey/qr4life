76d0423c7b8cb6c5e381afb2bcdb9e3433692c65
Hisham Mardam-Bey <mardambey@users.noreply.github.com>
2024-06-16 18:55:56 -0400

    Add GA, title, and blurb.

--- config.js
+++ config.js
@@ -1,3 +1,23 @@
+// Define the Google Analytics ID
+const googleAnalyticsId = 'YOUR_GOOGLE_ANALYTICS_ID';
+
+// Load the Google Analytics script dynamically
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', googleAnalyticsId);
+    };
+})();
+
 // Define the URL map and logos based on IDs
 const urlMap = {
     '1': { url: 'https://hisham.cc/', logo: 'https://www.gravatar.com/avatar/8ac7de375b1816679c3095f475f3a7f8.png', name: 'Blog' },
@@ -6,3 +26,7 @@ const urlMap = {
     '4': { url: 'https://linkedin.com/in/hishammardambey', logo: 'https://media.licdn.com/dms/image/C4E03AQEK--mPgGsg4w/profile-displayphoto-shrink_800_800/0/1582474785120?e=1724284800&v=beta&t=qHcBK8q7GDCUjkPBVcWsa9F7OXG3AkU0nKv24861LY8', name: 'LinkedIn' },                
     // Add more mappings as needed
 };
+
+// Define the title and blurb
+const gridTitle = "Hisham's Links";
+const gridBlurb = 'Welcome to my link directory. Please choose a link to go to or use the email address below to reach me.';
