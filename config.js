1245b2bcb81f1632b35b1e148c38192b96aa6d51
Hisham Mardam-Bey <mardambey@users.noreply.github.com>
2024-06-16 18:28:52 -0400

    Added config.

---
+++ config.js
@@ -0,0 +1,8 @@
+// Define the URL map and logos based on IDs
+const urlMap = {
+    '1': { url: 'https://hisham.cc/', logo: 'https://www.gravatar.com/avatar/8ac7de375b1816679c3095f475f3a7f8.png', name: 'Blog' },
+    '2': { url: 'https://hmb.capital', logo: 'https://via.placeholder.com/100?text=Logo2', name: 'HMB Capital' },
+    '3': { url: 'https://github.com/mardambey/', logo: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg', name: 'GitHub' },
+    '4': { url: 'https://linkedin.com/in/hishammardambey', logo: 'https://media.licdn.com/dms/image/C4E03AQEK--mPgGsg4w/profile-displayphoto-shrink_800_800/0/1582474785120?e=1724284800&v=beta&t=qHcBK8q7GDCUjkPBVcWsa9F7OXG3AkU0nKv24861LY8', name: 'LinkedIn' },                
+    // Add more mappings as needed
+};
